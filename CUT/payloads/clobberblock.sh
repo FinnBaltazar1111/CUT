4376d36699ac6b65a59d5420b457c14d434dea77
ERROR404NULL <error404nullnotfound@disroot.org>
2024-10-10 17:14:22 -0500

    Hannah got the ordering wrong

--- CUT/payloads/clobberblock.sh
+++ CUT/payloads/clobberblock.sh
@@ -7,7 +7,6 @@ echo "This script will remove the secondary kernel and rootfs utilized for Chrom
 echo "${red}You will not be able to undo this without restoring with a recovery USB${white}"
 echo "Press enter to continue, ctrl+C to cancel"
 read a
-yes | mkfs.ext4 /dev/mmcblk0p1
 cgpt add /dev/mmcblk0 -i 2 -P 10 -T 5 -S 1
 cat << EOF | fdisk /dev/mmcblk0
 d
@@ -25,5 +24,6 @@ n
 
 w
 EOF
+yes | mkfs.ext4 /dev/mmcblk0p1
 echo "Success! Your Chromebook can now no longer update"
 read a
